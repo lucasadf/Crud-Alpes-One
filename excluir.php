3ff0af8bd748bdb756ccb979f8a2fe0ab057c7e5
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 20:19:13 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,6 +1,6 @@
 <?php
 
-  $produto = $_GET['produto'];
-  $result = mysqli_query($mysqli, "DELETE FROM ESTOQUE WHERE NOME_PRODUTO=$produto");
+  $nome_produto = $_GET['nome_produto'];
+  $result = mysqli_query($mysqli, "DELETE FROM ESTOQUE WHERE NOME_PRODUTO=$nome_produto");
 
 ?>
