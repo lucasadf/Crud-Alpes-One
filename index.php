ab2d3b23e1a6df9a78bdc757ad5dae43ae6c5a18
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 19:51:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM ESTOQUE ORDER BY PRODUTO");
         <?php 
         while($res = mysqli_fetch_array($result)) {         
             echo "<tr>";
-            echo "<td>".$res['name']."</td>";
-            echo "<td>".$res['age']."</td>";   
+            echo "<td>".$res['nome_produto']."</td>";
+            echo "<td>".$res['qt_produto']."</td>";   
             echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
         }
         ?>
