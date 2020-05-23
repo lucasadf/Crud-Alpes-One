4ca756bbfe22e40f366bfce87cf6363177cc4824
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 19:58:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM ESTOQUE ORDER BY PRODUTO");
             echo "<tr>";
             echo "<td>".$res['nome_produto']."</td>";
             echo "<td>".$res['qt_produto']."</td>";   
-            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+            echo "<td><a href=\"edit.php?id=$res[id]\">Editar</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Deseja deletar?')\">Deletar</a></td>";        
         }
         ?>
     </table>
