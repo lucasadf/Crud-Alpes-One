79fb39af067266cc91a8627d5822a0e6dd0f39de
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 20:59:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
-<?php
-function banco() 
-{
-    $mysqli = new mysqli("localhost", "root", "", "ESTOQUE");
-    return $mysqli;
-}
-banco()
-$result = mysqli_query($mysqli, "SELECT * FROM ESTOQUE ORDER BY NOME_PRODUTO");
-?>
- 
 <html lang="pt">
 <head>    
-    <title>Estoque</title>
+
 </head>
  
 <body>
@@ -23,12 +13,20 @@ $result = mysqli_query($mysqli, "SELECT * FROM ESTOQUE ORDER BY NOME_PRODUTO");
             <td>Ação</td>
         </tr>
         <?php 
-        while($res = mysqli_fetch_array($result)) {         
+          function banco() 
+          {
+            $mysqli = new mysqli("localhost", "root", "", "ESTOQUE");
+            return $mysqli;
+          }
+          banco()
+          $result = mysqli_query($mysqli, "SELECT * FROM ESTOQUE ORDER BY NOME_PRODUTO");
+          while($res = mysqli_fetch_array($result)) 
+              {         
             echo "<tr>";
             echo "<td>".$res['nome_produto']."</td>";
             echo "<td>".$res['qt_produto']."</td>";   
             echo "<td><a href=\"edit.php?id=$res[id]\">Editar</a> | <a href=\"delete.php?id=$res[nome_produto]\" onClick=\"return confirm('Deseja deletar?')\">Deletar</a></td>";        
-        }
+              }
         ?>
     </table>
 </body>
