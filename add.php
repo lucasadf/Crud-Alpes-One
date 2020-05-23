b73d1c3fbb4266badd3d9101dee435afcc299253
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 20:55:21 -0300

    Create add.php

---
+++ add.php
@@ -0,0 +1,25 @@
+<?php
+  $nome_produto = $_GET['nome_produto'];
+  $qt_produto   = $_GET['qt_produto'];
+  $result = mysqli_query($mysqli, "INSERT INTO ESTOQUE(nome_produto,qt_produto) VALUES('$nome_produto','$qt_produto',)");
+?>
+
+<html>
+<head>
+
+</head>
+<body>
+  <table>
+    <tbody>
+      <tr>
+        <td>Produto: </td>
+        <td><input type="text" nome_produto="nome" value=""></td>
+      </tr>
+      <tr>
+        <td>Quantidade: </td>
+        <td><input type="text" qt_produto="qt_produto" value=""></td>
+      </tr>
+    </tbody>
+  </table> 
+</body>
+</html>
