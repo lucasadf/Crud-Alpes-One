c75ccf280903155d7529fe802ec531473542a1af
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-23 21:07:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
             echo "<tr>";
             echo "<td>".$res['nome_produto']."</td>";
             echo "<td>".$res['qt_produto']."</td>";   
-            echo "<td><a href=\"edit.php?id=$res[id]\">Editar</a> | <a href=\"delete.php?id=$res[nome_produto]\" onClick=\"return confirm('Deseja deletar?')\">Deletar</a></td>";        
+            echo "<td><a href=\"edit.php?id=$res[id]\">Editar</a> | <a href=\"delete.php?nome_produto=$res[nome_produto]\" onClick=\"return confirm('Deseja deletar?')\">Deletar</a></td>";        
               }
         ?>
     </table>
