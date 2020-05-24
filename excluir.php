39f695e46473311b2706cd5a2e8787ac2f23d0d6
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-24 11:45:29 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -7,7 +7,7 @@
   banco();
 
   $nome_produto = $_GET['nome_produto'];
-  $result = mysqli_query($mysqli, "DELETE FROM ESTOQUE WHERE NOME_PRODUTO=$nome_produto");
+  $result = mysqli_query($mysqli, "DELETE FROM PRODUTO_ESTOQUE WHERE NOME_PRODUTO=$nome_produto");
 
   echo "Dados deletados com sucesso";
   header("Location: index.php");
