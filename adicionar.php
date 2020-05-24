2113803fbb967ac815918e76d2826bdb2fbbc609
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-24 10:12:57 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -1,25 +1,16 @@
-<?php
-  $nome_produto = $_GET['nome_produto'];
-  $qt_produto   = $_GET['qt_produto'];
-  $result = mysqli_query($mysqli, "INSERT INTO ESTOQUE(nome_produto,qt_produto) VALUES('$nome_produto','$qt_produto',)");
-?>
-
 <html>
+  
 <head>
 
 </head>
+  
 <body>
-  <table>
-    <tbody>
-      <tr>
-        <td>Produto: </td>
-        <td><input type="text" nome_produto="nome_produto" value=""></td>
-      </tr>
-      <tr>
-        <td>Quantidade: </td>
-        <td><input type="text" qt_produto="qt_produto" value=""></td>
-      </tr>
-    </tbody>
-  </table> 
+  
+<?php
+  $nome_produto = $_POST['nome_produto'];
+  $qt_produto   = $_POST['qt_produto'];
+  $result = mysqli_query($mysqli, "INSERT INTO ESTOQUE(nome_produto,qt_produto) VALUES('$nome_produto','$qt_produto',)");
+?>  
+
 </body>
 </html>
