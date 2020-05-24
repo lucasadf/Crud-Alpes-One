32029fc7529ce53c00ddfd4ef38bc9ee6c720fa0
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-24 10:25:34 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -7,9 +7,27 @@
 <body>
   
 <?php
+  function banco() 
+  {
+    $mysqli = new mysqli("localhost", "root", "", "ESTOQUE");
+    return $mysqli;
+  }
+  
+  banco();
+  
   $nome_produto = $_POST['produto'];
   $qt_produto   = $_POST['quantidade'];
-  $result = mysqli_query($mysqli, "INSERT INTO ESTOQUE(nome_produto,qt_produto) VALUES('$nome_produto','$qt_produto',)");
+  
+  if(empty($nome_produto) || empty($qt_produto)) 
+  {
+    echo "Favor prencheer todos os campos";
+    echo <a href="index.php">Home</a>;
+  }
+  
+ else
+  {  
+    $result = mysqli_query($mysqli, "INSERT INTO ESTOQUE(nome_produto,qt_produto) VALUES('$nome_produto','$qt_produto',)");
+  } 
 ?>  
 
 </body>
