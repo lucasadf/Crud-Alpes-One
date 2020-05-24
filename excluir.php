4667d466a4363336ae1b53513616d8c0f5ec16ee
lucasadf <65832556+lucasadf@users.noreply.github.com>
2020-05-24 11:29:47 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,6 +1,15 @@
 <?php
+  function banco() 
+  {
+    $mysqli = new mysqli("localhost", "root", "", "ESTOQUE");
+    return $mysqli;
+  } 
+  banco();
 
   $nome_produto = $_GET['nome_produto'];
   $result = mysqli_query($mysqli, "DELETE FROM ESTOQUE WHERE NOME_PRODUTO=$nome_produto");
+
+  echo "Dados deletados com sucesso";
+  header("Location: index.php");
 
 ?>
